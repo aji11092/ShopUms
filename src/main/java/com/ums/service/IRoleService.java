7d335e5acba56cfbab69720dc6c516b247f2623a
ajimonstratotek <ajimon.m@stratotekcg.com>
2022-01-03 22:48:24 +0530

    Revert "initial push"
    This reverts commit a57ec592ad31b2d9796f52642c9a5efa6dabb954.

--- src/main/java/com/ums/service/IRoleService.java
+++
@@ -1,6 +0,0 @@
-package com.ums.service;
-
-public interface IRoleService<T> extends IService<T> {
-
-	T findByName(String name);
-}
--- src/main/java/com/ums/utils/ConstantUtils.java
+++
@@ -1,5 +0,0 @@
-package com.ums.utils;
-
-public enum ConstantUtils {
-	ADMIN, USER
-}
